cd47fe5a366273fe19186127cebfb94cff2294ad
handred800 <handred800@gmail.com>
2020-12-10 18:21:56 +0800

    新增文章搜尋功能

--- src/views/articles.vue
+++ src/views/articles.vue
@@ -1,23 +1,39 @@
 <template>
-  <div class="row" v-if="articlesData.length > 0">
-    <div class="col-33" v-for="(data,index) in articlesData" :key="index">
-      <a class="card card-hoverable" :href="data.url">
-        <div class="card-title">{{data.title}}</div>
-        <div class="card-meta">
-          <span>{{data.meta.date}}</span>
-          <span>gp: {{data.meta.gp}}</span>
-          <span>瀏覽: {{data.meta.view}}</span>
-        </div>
-      </a>
-    </div>
+<div>
+  <input type="text" class="form-input" placeholder="搜尋關鍵字" v-model="keyword">
+  <div v-if="articlesData.length > 0">
+    <transition-group name="article-list" class="row">
+      <div class="col-33 article-list" v-for="(data,index) in articleListFilted" :key="`${data.meta.date}-${index}`">
+        <a class="card card-hoverable" :href="data.url" target="_blank">
+          <div class="card-title">{{data.title}}</div>
+          <div class="card-meta">
+            <span>{{data.meta.date}}</span>
+            <span>gp: {{data.meta.gp}}</span>
+            <span>瀏覽: {{data.meta.view}}</span>
+          </div>
+        </a>
+      </div>
+    </transition-group>
   </div>
+</div>
 </template>
 
 <script>
 export default {
   props: ['articlesData'],
   data() {
-    return {};
+    return {
+      keyword: '',
+    };
+  },
+  computed: {
+    articleListFilted() {
+      const vm = this;
+      const articles = vm._.cloneDeep(vm.articlesData);
+
+      if (vm.keyword === '') return articles;
+      return articles.filter((article) => article.title.toLowerCase().includes(vm.keyword.toLowerCase()));
+    },
   },
 };
 </script>
